cf0025d229893b2cff3737c7bc325516cc53af3b
KonzernTechie <kv.nijesh@gmail.com>
2020-10-17 00:54:00 +0530

    outline of the project

--- issuetracker/src/main/java/com/tcsglobal/issuetracker/controller/WelcomeController.java
+++ issuetracker/src/main/java/com/tcsglobal/issuetracker/controller/WelcomeController.java
@@ -3,10 +3,45 @@
  */
 package com.tcsglobal.issuetracker.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
  * @author Apple
  *
  */
+@Controller
 public class WelcomeController {
 
+	
+    // inject via application.properties
+    @Value("${it.message.welcome}")
+    private String message;
+
+    private List<String> tasks = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
+
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("message", message);
+        model.addAttribute("tasks", tasks);
+
+        return "welcome"; //view
+    }
+
+    // /hello?name=kotlin
+    @GetMapping("/hello")
+    public String mainWithParam(
+            @RequestParam(name = "name", required = false, defaultValue = "")
+			String name, Model model) {
+
+        model.addAttribute("message", name);
+
+        return "welcome"; //view
+    }
 }
